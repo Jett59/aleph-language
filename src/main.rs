67c99e7010ddf4bee61d5814b8dcd2b9c8c3105b
jett59 <jett.thompson.59@gmail.com>
2024-09-21 18:19:47 +1000

    add the option to parse a file from the command line

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,9 @@ fn parse_top_level(input: &str) -> IResult<&str, Vec<TopLevelStatement>> {
 }
 
 fn main() {
-    let input = "f : Real -> Real";
-    let result = parse_top_level(input);
-    println!("{:?}", result);
+    let args: Vec<String> = std::env::args().collect();
+    let file_name = &args[1];
+    let input = std::fs::read_to_string(file_name).unwrap();
+    let (_, top_level) = parse_top_level(&input).unwrap();
+    println!("{:?}", top_level);
 }
